7fc7c24dd25662432af97051733321498b997385
Idan Shalem <idanshalem25@gmail.com>
2020-08-18 11:25:09 +0300

    Uncommented all of excercises

--- Day-3/main.js
+++ Day-3/main.js
@@ -1,66 +1,66 @@
-// // ex-1
-// const push = function () {
-//     console.log("pushing it!")
-//   }
+// ex-1
+const push = function () {
+    console.log("pushing it!")
+  }
   
-//   const pull = function () {
-//     console.log("pulling it!")
-//   }
+  const pull = function () {
+    console.log("pulling it!")
+  }
   
-//   const pushPull = function(func) {
-//       func()
-//   }
+  const pushPull = function(func) {
+      func()
+  }
 
-//   pushPull(push) //should print "pushing it!"
-//   pushPull(pull) //should print "pulling it!"
+  pushPull(push) //should print "pushing it!"
+  pushPull(pull) //should print "pulling it!"
 
-// // ex-2
-// const returnTime = function (time) {
-//     alert('The current time is: ' + time)
-//   }
+// ex-2
+const returnTime = function (time) {
+    alert('The current time is: ' + time)
+  }
   
-//   const getTime = function(func){
-//       const time = new Date()
-//       func(time)
-//   }
-//   getTime(returnTime)
+  const getTime = function(func){
+      const time = new Date()
+      func(time)
+  }
+  getTime(returnTime)
 
-// // Ex-3
-// const logData = function(data){
-//     console.log(data)
-// }
+// Ex-3
+const logData = function(data){
+    console.log(data)
+}
 
-// const displayData = function (alertDataFunc, logDataFunc, data) {
-//     alertDataFunc(data);
-//     logDataFunc(data);
-//   };
+const displayData = function (alertDataFunc, logDataFunc, data) {
+    alertDataFunc(data);
+    logDataFunc(data);
+  };
   
-// displayData(alert, logData, "I like to party")
+displayData(alert, logData, "I like to party")
 
-// // Ex-4
-// const returnSum = (n1, n2, n3) => n1 + n2 + n3
+// Ex-4
+const returnSum = (n1, n2, n3) => n1 + n2 + n3
 
-// console.log(returnSum(1,2,3))
+console.log(returnSum(1,2,3))
 
-// Ex-5
-// const capitalize = name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+Ex-5
+const capitalize = name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
 
-// console.log(capitalize("bOb")) // returns Bob
-// console.log(capitalize("TAYLOR")) // returns Taylor
-// console.log(capitalize("feliSHIA") )// returns Felishia
+console.log(capitalize("bOb")) // returns Bob
+console.log(capitalize("TAYLOR")) // returns Taylor
+console.log(capitalize("feliSHIA") )// returns Felishia
 
-// // Ex-6
-// const determineWeather = temp => {
-//     if(temp > 25){
-//       return "hot"
-//     }
-//     return "cold"
-//   }
+// Ex-6
+const determineWeather = temp => {
+    if(temp > 25){
+      return "hot"
+    }
+    return "cold"
+  }
   
-//   const commentOnWeather = temp => `It's ${determineWeather(temp)}`
+  const commentOnWeather = temp => `It's ${determineWeather(temp)}`
 
-// console.log(commentOnWeather(30)) //returns "It's hot"
-// console.log(commentOnWeather(22)) //returns "It's cold"
+console.log(commentOnWeather(30)) //returns "It's hot"
+console.log(commentOnWeather(22)) //returns "It's cold"
 
 // Ex-7
 const explode = (lightFunc, soundFunc, sound) => {
